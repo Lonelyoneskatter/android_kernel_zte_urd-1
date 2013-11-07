f326192403c2833bc4a714a8c62a2c27bbe10ce8
Dan Carpenter <dan.carpenter@oracle.com>
2013-11-07 11:04:20 +0300

    mfd: pm8921: Potential NULL dereference in pm8921_remove()
    commit d6daef95127e41233ac8e2d8472d8c0cd8687d38 upstream.
    
    We assume that "pmic" could be NULL and then dereference it two lines
    later.  I fix this by moving the dereference inside the NULL check.
    
    Fixes: c013f0a56c56 ('mfd: Add pm8xxx irq support')
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- drivers/mfd/pm8921-core.c
+++ drivers/mfd/pm8921-core.c
@@ -952,10 +952,7 @@ static int pm8921_remove(struct platform_device *pdev)
 	if (drvdata)
 		pmic = drvdata->pm_chip_data;
 	if (pmic) {
-		if (pmic->dev)
 			mfd_remove_devices(pmic->dev);
-		if (pmic->irq_chip)
-			pm8xxx_irq_exit(pmic->irq_chip);
 		if (pmic->mfd_regulators) {
 			for (i = 0; i < ARRAY_SIZE(regulator_data); i++)
 				mutex_destroy(&regulator_data[i].pc_lock);
@@ -966,6 +963,12 @@ static int pm8921_remove(struct platform_device *pdev)
 		kfree(pmic->mfd_regulators);
 		kfree(pmic->regulator_cdata);
 		kfree(pmic);
+
+		mfd_remove_devices(pmic->dev);
+		if (pmic->irq_chip) {
+			pm8xxx_irq_exit(pmic->irq_chip);
+			pmic->irq_chip = NULL;
+		}
 	}
 	platform_set_drvdata(pdev, NULL);
 
