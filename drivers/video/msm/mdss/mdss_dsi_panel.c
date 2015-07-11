9cfcfe30f55d600a827fe920ee94601f27032993
Steve Kondik <steve@cyngn.com>
2015-07-11 05:49:30 -0700

    video: mdss: LiveDisplay driver
     * Provide a simple color temperature adjustment interface via MDSS PCC.
     * Provide hardware integration support for CABC, SRE, and CE.
    
    Change-Id: I4ed1f3876767dd6bedccaf0bbf61af7d36730ca0

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -24,6 +24,7 @@
 
 #include "mdss_dsi.h"
 #include "mdss_dba_utils.h"
+#include "mdss_livedisplay.h"
 
 /* start -20160505-ENABLE_LCD_MIPI_DEBUG*/
 #ifdef ENABLE_LCD_MIPI_DEBUG
@@ -213,7 +214,7 @@ u32 mdss_dsi_panel_cmd_read(struct mdss_dsi_ctrl_pdata *ctrl, char cmd0,
 	return 0;
 }
 
-static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds)
 {
 	struct dcs_cmd_req cmdreq;
@@ -1385,6 +1386,7 @@ static int mdss_dsi_post_panel_on(struct mdss_panel_data *pdata)
 	}
 	
 	display_on = false;
+        mdss_livedisplay_update(ctrl, MODE_UPDATE_ALL);
 
 end:
 	pr_debug("%s:-\n", __func__);
@@ -1537,7 +1539,7 @@ static void mdss_dsi_parse_trigger(struct device_node *np, char *trigger,
 }
 
 
-static int mdss_dsi_parse_dcs_cmds(struct device_node *np,
+int mdss_dsi_parse_dcs_cmds(struct device_node *np,
 		struct dsi_panel_cmds *pcmds, char *cmd_key, char *link_key)
 {
 	const char *data;
@@ -3036,6 +3038,9 @@ static int mdss_panel_parse_dt(struct device_node *np,
 	get_default_panel_setting(pinfo,&lcd_mipi_debug);
 #endif
 	/* end -20160505-ENABLE_LCD_MIPI_DEBUG*/
+
+        mdss_livedisplay_parse_dt(np, pinfo);
+
 	return 0;
 
 error:
