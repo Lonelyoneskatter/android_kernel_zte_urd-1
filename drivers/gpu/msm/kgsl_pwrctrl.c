eb29844a67d6ba1105789078f0769a0e19c2d595
Lonelyoneskatter <threesixoh.skater@yahoo.com>
2016-04-16 14:06:00 -0700

    kgsl_pwrctrl: Fixed super old bug in gpu thermal pwr level.
    Signed-off-by: Lonelyoneskatter <threesixoh.skater@yahoo.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -536,7 +536,17 @@ static ssize_t kgsl_pwrctrl_thermal_pwrlevel_store(struct device *dev,
 	if (level > pwr->num_pwrlevels - 2)
 		level = pwr->num_pwrlevels - 2;
 
-	pwr->thermal_pwrlevel = level;
+	/* GPU fix */
+	if (level > 7)
+		level = 7;
+
+	if (level == 0)
+		pwr->thermal_pwrlevel = level;
+	else if (level == 1)
+		pwr->thermal_pwrlevel = level;
+	else
+		pwr->thermal_pwrlevel = level - 2;
+	/* End */
 
 	/* Update the current level using the new limit */
 	kgsl_pwrctrl_pwrlevel_change(device, pwr->active_pwrlevel);
