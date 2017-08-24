857163e52a33ca97684a7600a5af96e71c0ada39
Lonelyoneskatter <threesixoh.skater@yahoo.com>
2017-08-23 17:26:26 -0700

    powersuspend: add back hybrid & autosleep hooks.  * Set hybrid to default.  * Bump to version 1.7.1. Signed-off-by: Lonelyoneskatter <threesixoh.skater@yahoo.com>

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -9,6 +9,9 @@
 #include <linux/device.h>
 #include <linux/mutex.h>
 #include <linux/pm_wakeup.h>
+#ifdef CONFIG_POWERSUSPEND		
+#include <linux/powersuspend.h>		
+#endif
 
 #include "power.h"
 #include <linux/fb.h>//ZTE_PM to record how long LCD keeps on
@@ -115,9 +118,16 @@ pr_info("pm_autosleep_set_state state %d \n",state);
 	if (state > PM_SUSPEND_ON) {
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
+#ifdef CONFIG_POWERSUSPEND		
+		// Yank555.lu : add hook to handle powersuspend tasks (sleep)		
+		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE);		
+#endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
-
+#ifdef CONFIG_POWERSUSPEND		
+		// Yank555.lu : add hook to handle powersuspend tasks (wakeup)		
+		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_INACTIVE);		
+#endif
 	}
 
 	mutex_unlock(&autosleep_lock);
